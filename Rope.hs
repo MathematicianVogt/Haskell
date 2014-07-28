4d52192ad4f8288b990e9ae7f094f627c031bdd0
MathematicianVogt <MathematicianVogt@Ryans-MacBook-Pro-3.local>
2014-07-28 00:01:31 -0400

    update

--- Rope.hs
+++ Rope.hs
@@ -5,9 +5,12 @@
 
 FOR GRADER:
 
-Two issues, 
+three issues, 
 -having to do with dissect, putting quotes into string with the escape character is including the escape character
 -return of dissect has strings around it
+-my exception for out of bounds isnt being called correctly for some reason i tried to trouble shoot it locally by going step by step and it worked
+ but on test failed.
+
 
 Not sure how to remedy those, its some sort of weird type thing.
 
@@ -43,11 +46,6 @@ concatt a b = a ++ b
 theLength :: String -> Integer
 theLength (x:xs) = 1+ (theLength xs)
 theLength [] = 0
---gets the length of a rope object
-sizeOf :: Rope -> Integer
-sizeOf (TextRope s) = theLength s
-sizeOf (ConcatRope a b) = sizeOf(a) +sizeOf (b)
-sizeOf (SubRope _ _ b) =b
 
 --get the specific character in a string at element i
 getCharac :: [Char]->Integer-> Char
@@ -57,26 +55,18 @@ getCharac (x:xs) i = getCharac xs (i-1)
 
 
 
---gets the ith character in a rope 
-i_th :: Rope-> Integer -> Char
-i_th (TextRope s) i  =getCharac s i 
-i_th (ConcatRope a b) i = if( i> (sizeOf a)-1)
-							then i_th b (i - (sizeOf a))
-							else i_th a i
-i_th (SubRope a start end) i = i_th a (start+i)
-
---i_th (ConcatRope a b) i = 
-
 --returns a string representation of the datastructure.
-dissect :: Rope -> [Char]
+dissect :: Rope -> String
 dissect (TextRope s)= "TextRope(\"" ++ ""  ++ s ++ "\")"
 dissect (ConcatRope a b) = "ConcatRope(" ++ (dissect a) ++ "," ++ (dissect b) ++ ")"
-dissect (SubRope a b c) = "SubRope(" ++ (dissect a) ++ "," ++ [(intToDigit (fromIntegral b))] ++ "," ++ [(intToDigit (fromIntegral c ))] ++ ")"
+dissect (SubRope a b c) = "SubRope(" ++ (dissect a) ++ "," ++ (show b) ++ "," ++ (show c) ++ ")"
 
 buildString ::Rope->Integer-> Integer->String
 buildString rope 0 0 = []
 buildString rope start 0 = []
-buildString rope start end = [(i_th rope start)] ++ (buildString rope (start+1) (end-1))  
+buildString rope start end =if((start+end-1)>((sizeOf rope)-1))
+								then error ("index " ++ (show end) ++ " out of bounds.") 
+							    else [(i_th rope start)] ++ (buildString rope (start+1) (end-1))  
 
 
 
@@ -84,6 +74,28 @@ buildString rope start end = [(i_th rope start)] ++ (buildString rope (start+1) 
 instance Show Rope where
     show (TextRope s) = s
     show (ConcatRope a b) = show a ++ show b
-    show (SubRope rope start end) = buildString rope start end    
+    show (SubRope rope start end) = buildString rope start end 
+
+
+instance Linear Rope where
+	--gets the ith character in a rope 
+	i_th (TextRope s) i  =getCharac s i 
+	i_th (ConcatRope a b) i = if( i> (sizeOf a)-1)
+								then i_th b (i - (sizeOf a))
+								else i_th a i
+	i_th (SubRope a start end) i = i_th a (start+i)
+
+	--gets the length of a rope object
+	sizeOf (TextRope s) = theLength s
+	sizeOf (ConcatRope a b) = sizeOf(a) +sizeOf (b)
+	sizeOf (SubRope _ _ b) =b
+
+class Linear a where
+	i_th :: a-> Integer -> Char
+	sizeOf :: a -> Integer
+
+
+
+
    
     
--- RungeKutta.hs
+++ RungeKutta.hs
@@ -1,5 +1,6 @@
 --Runge Kutta solving differential equations
 makeGrid start end h = [start,(start+h)..end]
+returnTail (x:xs) = xs
 
 rungeKuttaSolver diffEq [] yList h = yList
 rungeKuttaSolver diffEq (tn:ts) (yn:ys) h = rungeKuttaSolver diffEq ts ( (yn  + (h/6)*(k1 + 2*k2 + 2*k3 + k4)): (yn:ys)) h
@@ -9,6 +10,7 @@ rungeKuttaSolver diffEq (tn:ts) (yn:ys) h = rungeKuttaSolver diffEq ts ( (yn  + 
 											k3= diffEq (tn + (h/2)) (yn + (h/2)*k2)
 											k4 = diffEq (tn + h) (yn+(h*k3))
 
-rungeKutta1 diffEq (x:xs) yList h = rungeKuttaSolver diffEq xs yList h
 
-rungeKutta diffEq t0 t1 y0 n = reverse( rungeKutta1 diffEq (makeGrid t0 t1 ((t1-t0)/n)) [y0] ((t1-t0)/n))
+rungeKutta diffEq t0 t1 y0 n = reverse( rungeKuttaSolver diffEq (returnTail (makeGrid t0 t1 ((t1-t0)/n))) [y0] ((t1-t0)/n))
+
+
